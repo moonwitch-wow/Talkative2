422014157aab5b884768fc0075f8ff92daa31b15
moonwitch <kelly.crabbe@gmail.com>
2014-10-19 19:28:31 +0200

    added sticky chat

---
+++ sticky.lua
@@ -0,0 +1,15 @@
+------------------------------------------------------------------------
+-- Sticky channels
+------------------------------------------------------------------------
+ChatTypeInfo.CHANNEL.sticky = 0
+ChatTypeInfo.WHISPER.sticky = 1
+ChatTypeInfo.BN_WHISPER.sticky = 1
+ChatTypeInfo.BN_CONVERSATION.sticky = 1
+ChatTypeInfo.PARTY.sticky = 1
+ChatTypeInfo.RAID.sticky = 1
+ChatTypeInfo.GUILD.sticky = 1
+ChatTypeInfo.OFFICER.sticky = 1
+ChatTypeInfo.REPLY.sticky = 1
+ChatTypeInfo.INSTANCE_CHAT.sticky = 1
+ChatTypeInfo.INSTANCE_CHAT_LEADER.sticky = 1
+ChatTypeInfo.PARTY_LEADER.sticky = 1
