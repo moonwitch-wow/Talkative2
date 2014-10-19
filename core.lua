f89ac1a75b44a2775804dcff954e4b4339b602d9
moonwitch <kelly.crabbe@gmail.com>
2014-10-19 19:29:58 +0200

    event handler and basic loadr

---
+++ core.lua
@@ -0,0 +1,32 @@
+------------------------------------------------------------------------
+-- Setting up the local scope
+------------------------------------------------------------------------
+local _, Talkative2 = ...
+local handler = CreateFrame("Frame")
+
+------------------------------------------------------------------------
+-- Functions
+------------------------------------------------------------------------
+function Talkative2.PLAYER_ENTERING_WORLD(...)
+  Talkative2.Init()
+  print('Talkative2 Loaded') -- debugging purposes
+end
+
+function Talkative2.PLAYER_LOGIN(...)
+  Talkative2.Tabify()
+end
+
+function Talkative2.PLAYER_LEAVING_WORLD(...)
+ -- handle PLAYER_LEAVING_WORLD here
+end
+
+------------------------------------------------------------------------
+-- Many Whelps, handle Events
+------------------------------------------------------------------------
+handler:SetScript('OnEvent', function(self, event, ...)
+  Talkative2[event](self, event, ...)
+end)
+
+for k, v in pairs(Talkative2) do
+  handler:RegisterEvent(k) -- Register all events for which handlers have been defined
+end
