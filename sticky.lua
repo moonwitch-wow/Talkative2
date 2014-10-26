6ddc4c1239d98ddb19aa354f036ef2bf7407dd39
moonwitch <kelly.crabbe@gmail.com>
2014-10-26 10:29:47 +0100

    will consolidate

--- sticky.lua
+++ sticky.lua
@@ -13,3 +13,15 @@ ChatTypeInfo.REPLY.sticky = 1
 ChatTypeInfo.INSTANCE_CHAT.sticky = 1
 ChatTypeInfo.INSTANCE_CHAT_LEADER.sticky = 1
 ChatTypeInfo.PARTY_LEADER.sticky = 1
+
+ChatTypeInfo["WHISPER"].flashTab = true
+ChatTypeInfo["GUILD"].flashTab = true
+ChatTypeInfo["OFFICER"].flashTab = true
+ChatTypeInfo["RAID"].flashTab = true
+ChatTypeInfo["PARTY"].flashTab = true
+ChatTypeInfo["RAID_LEADER"].flashTab = true
+ChatTypeInfo["PARTY_LEADER"].flashTab = true
+ChatTypeInfo["INSTANCE_CHAT"].flashTab = true
+ChatTypeInfo["INSTANCE_CHAT_LEADER"].flashTab = true
+ChatTypeInfo["BN_WHISPER"].flashTab = true
+ChatTypeInfo["BN_CONVERSATION"].flashTab = true
