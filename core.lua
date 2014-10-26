e356295cd9e4880b85c9a61895d0e098bc14b23e
moonwitch <kelly.crabbe@gmail.com>
2014-10-26 02:25:11 +0200

    changed event for init() and added a few events

--- core.lua
+++ core.lua
@@ -8,22 +8,28 @@ local handler = CreateFrame("Frame")
 -- Functions
 ------------------------------------------------------------------------
 function Talkative2.PLAYER_ENTERING_WORLD(...)
-  Talkative2.Init()
   print('Talkative2 Loaded') -- debugging purposes
 end
 
 function Talkative2.PLAYER_LOGIN(...)
   Talkative2.Tabify()
+  Talkative2.Init()
 end
 
 function Talkative2.PLAYER_LEAVING_WORLD(...)
  -- handle PLAYER_LEAVING_WORLD here
 end
 
+-- Handler:RegisterEvent('PLAYER_LOGIN')
+-- Handler:RegisterEvent('CHAT_MSG_WHISPER')
+-- Handler:RegisterEvent('CHAT_MSG_BN_WHISPER')
+-- Handler:RegisterEvent('CHAT_MSG_BN_CONVERSATION')
+
 -- ClassColors
 function Talkative2.UPDATE_CHAT_COLOR_NAME_BY_CLASS(self,event,type,set,...)
   if not set then SetChatColorNameByClass(type,true); end
 end
+
 ------------------------------------------------------------------------
 -- Many Whelps, handle Events
 ------------------------------------------------------------------------
