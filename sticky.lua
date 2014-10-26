9f5150c4c8f4912df71357ace25369439e9d6d26
moonwitch <kelly.crabbe@gmail.com>
2014-10-26 12:13:31 +0100

    Consolidated and made it more streamlined.

--- sticky.lua
+++ sticky.lua
@@ -1,10 +1,6 @@
 ------------------------------------------------------------------------
 -- Sticky channels
 ------------------------------------------------------------------------
-ChatTypeInfo.CHANNEL.sticky = 0
-ChatTypeInfo.WHISPER.sticky = 1
-ChatTypeInfo.BN_WHISPER.sticky = 1
-ChatTypeInfo.BN_CONVERSATION.sticky = 1
 ChatTypeInfo.PARTY.sticky = 1
 ChatTypeInfo.RAID.sticky = 1
 ChatTypeInfo.GUILD.sticky = 1
@@ -14,14 +10,10 @@ ChatTypeInfo.INSTANCE_CHAT.sticky = 1
 ChatTypeInfo.INSTANCE_CHAT_LEADER.sticky = 1
 ChatTypeInfo.PARTY_LEADER.sticky = 1
 
-ChatTypeInfo["WHISPER"].flashTab = true
-ChatTypeInfo["GUILD"].flashTab = true
-ChatTypeInfo["OFFICER"].flashTab = true
-ChatTypeInfo["RAID"].flashTab = true
-ChatTypeInfo["PARTY"].flashTab = true
-ChatTypeInfo["RAID_LEADER"].flashTab = true
-ChatTypeInfo["PARTY_LEADER"].flashTab = true
-ChatTypeInfo["INSTANCE_CHAT"].flashTab = true
-ChatTypeInfo["INSTANCE_CHAT_LEADER"].flashTab = true
-ChatTypeInfo["BN_WHISPER"].flashTab = true
-ChatTypeInfo["BN_CONVERSATION"].flashTab = true
+ChatTypeInfo.WHISPER.flashTab = true
+ChatTypeInfo.GUILD.flashTab = true
+ChatTypeInfo.OFFICER.flashTab = true
+ChatTypeInfo.INSTANCE_CHAT.flashTab = true
+ChatTypeInfo.INSTANCE_CHAT_LEADER.flashTab = true
+ChatTypeInfo.BN_WHISPER.flashTab = true
+ChatTypeInfo.WHISPER.flashTab = true
