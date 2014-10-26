d447d1a9cc9bb4ad880fc67b647d13fe5d99774d
moonwitch <kelly.crabbe@gmail.com>
2014-10-26 02:24:37 +0200

    changed the param to the skin-function in hopes of fixing the pet combat log. Alas..

--- visual.lua
+++ visual.lua
@@ -2,67 +2,67 @@
 -- Setting up the local scope
 ------------------------------------------------------------------------
 local _, Talkative2 = ...
+-- Table to keep track of frames you already saw:
+local frames = {}
 
 ------------------------------------------------------------------------
 -- Editbox
 ------------------------------------------------------------------------
-function Talkative2.skin(index)
-  local frame = _G['ChatFrame' .. index]
-  frame:SetFont(STANDARD_TEXT_FONT, 10, nil)
-  frame:SetShadowOffset(1, -1)
-  frame:SetSpacing(1.4)
-  -- FCF_SetChatWindowFontSize(nil, frame, 13) -- Change the chat frame font
-  frame:SetClampedToScreen(false) -- allows moving to edge of screen
-  frame:SetClampRectInsets(0,0,0,0) -- allows moving to edge of screen
+function Talkative2.skin(frame)
+  if frames[frame] then return end
 
-  FCF_SetWindowAlpha(frame, .1)
-  FCF_SetLocked(frame, 1)
+  for i = 1, NUM_CHAT_WINDOWS do
+    _G[frame:GetName() .. "ButtonFrame"]:Hide()
+    _G[frame:GetName() .. "EditBoxLeft"]:SetTexture(nil)
+    _G[frame:GetName() .. "EditBoxMid"]:SetTexture(nil)
+    _G[frame:GetName() .. "EditBoxRight"]:SetTexture(nil)
 
-  local editbox = _G['ChatFrame' .. index .. 'EditBox']
-  editbox:SetAltArrowKeyMode(false)
+    frame:SetFont(STANDARD_TEXT_FONT, 12, nil)
+    frame:SetShadowOffset(1, -1)
+    frame:SetSpacing(1.4)
+    FCF_SetChatWindowFontSize(nil, frame, 13) -- Change the chat frame font
+    frame:SetClampedToScreen(false) -- allows moving to edge of screen
+    frame:SetClampRectInsets(0,0,0,0) -- allows moving to edge of screen
 
-  editbox:ClearAllPoints()
-  editbox:SetPoint('BOTTOMRIGHT', frame, 'TOPRIGHT', 0, 20)
-  editbox:SetPoint('BOTTOMLEFT', frame, 'TOPLEFT', 0, 20)
-  editbox:SetFont(STANDARD_TEXT_FONT, 13, nil)
-  editbox:SetShadowOffset(1,-1)
+    FCF_SetWindowAlpha(frame, .1)
+    FCF_SetLocked(frame, 1)
 
-  editbox.focusLeft:SetTexture(nil)
-  editbox.focusMid:SetTexture(nil)
-  editbox.focusRight:SetTexture(nil)
+    local editbox = _G[frame:GetName() .. "EditBox"]
+    editbox:SetAltArrowKeyMode(false)
+    editbox:ClearAllPoints()
+    editbox:SetPoint('BOTTOMRIGHT', ChatFrame1, 'TOPRIGHT', 0, 20)
+    editbox:SetPoint('BOTTOMLEFT', ChatFrame1, 'TOPLEFT', 0, 20)
+    editbox:SetFont(STANDARD_TEXT_FONT, 13, nil)
+    editbox:SetShadowOffset(1,-1)
 
-  editbox.header:ClearAllPoints()
-  editbox.header:SetPoint('LEFT')
-  editbox.header:SetFont(STANDARD_TEXT_FONT, 13, nil)
-  editbox.header:SetShadowOffset(1, -1)
+    editbox.focusLeft:SetTexture(nil)
+    editbox.focusMid:SetTexture(nil)
+    editbox.focusRight:SetTexture(nil)
 
-  local orig = editbox.SetTextInsets
-  editbox.SetTextInsets = function(self)
-    orig(self, self.header:GetWidth(), 0, 0, 0)
+    editbox.header:ClearAllPoints()
+    editbox.header:SetPoint('LEFT')
+    editbox.header:SetFont(STANDARD_TEXT_FONT, 13, nil)
+    editbox.header:SetShadowOffset(1, -1)
+
+    local orig = editbox.SetTextInsets
+    editbox.SetTextInsets = function(self)
+      orig(self, self.header:GetWidth(), 0, 0, 0)
+    end
   end
-
-  _G['ChatFrame' .. index .. 'ButtonFrame']:Hide()
-  _G['ChatFrame' .. index .. 'EditBoxLeft']:SetTexture(nil)
-  _G['ChatFrame' .. index .. 'EditBoxMid']:SetTexture(nil)
-  _G['ChatFrame' .. index .. 'EditBoxRight']:SetTexture(nil)
+   frames[frame] = true
 end
 
-for index = 1, NUM_CHAT_WINDOWS do
-  if(index ~= 2) then
-    Talkative2.skin(index)
-
-    -- _G['ChatFrame' .. index .. 'Tab']:RegisterForClicks('LeftButtonUp', 'RightButtonUp')
-  end
+for i = 1, NUM_CHAT_WINDOWS do
+  Talkative2.skin(_G["ChatFrame" .. i])
 end
 
 ------------------------------------------------------------------------
 --skin temporary chats
 ------------------------------------------------------------------------
-hooksecurefunc("FCF_OpenTemporaryWindow", function()
-  for _, chatFrameName in pairs(CHAT_FRAMES) do
-    local frame = _G[chatFrameName]
-    if (frame.isTemporary) then
-      Talkative2.skin(index)
-    end
-  end
-end)
+-- Set up a dirty hook to catch temporary windows and customize them when they are created:
+local old_OpenTemporaryWindow = FCF_OpenTemporaryWindow
+FCF_OpenTemporaryWindow = function(...)
+    local frame = old_OpenTemporaryWindow(...)
+    Talkative2.skin(frame)
+    return frame
+end
