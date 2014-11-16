716787f3d0a6893e07a3b0e59155075e784edd6e
moonwitch <kelly.crabbe@gmail.com>
2014-11-16 15:02:42 +0100

    slight update to flashers

--- modules/sticky.lua
+++ modules/sticky.lua
@@ -1,19 +1,23 @@
 ------------------------------------------------------------------------
 -- Sticky channels
+-- Sticky - makes your next line be in that channel
+-- flashTab - makes the font 'flash' on new message
+-- flashTabOnGeneral - makes GEN flash as well. Only for real issues!!
 ------------------------------------------------------------------------
-ChatTypeInfo.PARTY.sticky = 1
-ChatTypeInfo.RAID.sticky = 1
-ChatTypeInfo.GUILD.sticky = 1
-ChatTypeInfo.OFFICER.sticky = 1
-ChatTypeInfo.REPLY.sticky = 1
-ChatTypeInfo.INSTANCE_CHAT.sticky = 1
-ChatTypeInfo.INSTANCE_CHAT_LEADER.sticky = 1
-ChatTypeInfo.PARTY_LEADER.sticky = 1
-
-ChatTypeInfo.WHISPER.flashTab = true
-ChatTypeInfo.GUILD.flashTab = true
-ChatTypeInfo.OFFICER.flashTab = true
-ChatTypeInfo.INSTANCE_CHAT.flashTab = true
-ChatTypeInfo.INSTANCE_CHAT_LEADER.flashTab = true
-ChatTypeInfo.BN_WHISPER.flashTab = true
-ChatTypeInfo.WHISPER.flashTab = true
+ChatTypeInfo["SYSTEM"]                 = { sticky = 0, flashTab = true, flashTabOnGeneral = true }
+ChatTypeInfo["SAY"]                    = { sticky = 0, flashTab = false, flashTabOnGeneral = false }
+ChatTypeInfo["PARTY"]                  = { sticky = 1, flashTab = true, flashTabOnGeneral = false }
+ChatTypeInfo["RAID"]                   = { sticky = 1, flashTab = true, flashTabOnGeneral = false }
+ChatTypeInfo["GUILD"]                  = { sticky = 1, flashTab = true, flashTabOnGeneral = false }
+ChatTypeInfo["OFFICER"]                = { sticky = 1, flashTab = true, flashTabOnGeneral = false }
+ChatTypeInfo["WHISPER"]                = { sticky = 1, flashTab = true, flashTabOnGeneral = false }
+ChatTypeInfo["WHISPER_INFORM"]         = { sticky = 0, flashTab = true, flashTabOnGeneral = false }
+ChatTypeInfo["REPLY"]                  = { sticky = 1, flashTab = true, flashTabOnGeneral = false }
+ChatTypeInfo["RAID_LEADER"]            = { sticky = 1, flashTab = true, flashTabOnGeneral = false }
+ChatTypeInfo["RAID_WARNING"]           = { sticky = 0, flashTab = true, flashTabOnGeneral = true }
+ChatTypeInfo["INSTANCE_CHAT"]          = { sticky = 1, flashTab = true, flashTabOnGeneral = false }
+ChatTypeInfo["INSTANCE_CHAT_LEADER"]   = { sticky = 1, flashTab = true, flashTabOnGeneral = true }
+ChatTypeInfo["PARTY_LEADER"]           = { sticky = 1, flashTab = true, flashTabOnGeneral = true }
+ChatTypeInfo["BN_WHISPER"]             = { sticky = 1, flashTab = true, flashTabOnGeneral = true }
+ChatTypeInfo["BN_WHISPER_INFORM"]      = { sticky = 0, flashTab = true, flashTabOnGeneral = false }
+ChatTypeInfo["BN_CONVERSATION"]        = { sticky = 1, flashTab = true, flashTabOnGeneral = false }
